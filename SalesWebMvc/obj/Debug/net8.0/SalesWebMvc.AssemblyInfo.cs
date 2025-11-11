7c7861eabd3f99520990a0e5cc3ead96db67af09
Luiz Baraúna <116582873+luizcbarauna@users.noreply.github.com>
2025-11-11 10:40:05 -0300

    Criando Construtor-Model-View Department

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4ea661eb265ef0ef33be941ae70e1e285844ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
